7b49443b893ccda21c405c361761296292565778
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 09:33:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+// Load JSON data
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Sort data by time
+    data.sort((a, b) => a.time - b.time);
+
+    // Add rows to table
+    const tbody = document.querySelector('#my-table tbody');
+    data.forEach((record, index) => {
+      // Create new row
+      const row = document.createElement('tr');
+
+      // Add columns to row
+      const rank = document.createElement('td');
+      rank.textContent = index + 1;
+      row.appendChild(rank);
+
+      const name = document.createElement('td');
+      name.textContent = record.name;
+      row.appendChild(name);
+
+      const time = document.createElement('td');
+      time.textContent = formatTime(record.time);
+      row.appendChild(time);
+
+      const status = document.createElement('td');
+      const statusText = document.createTextNode(record.status.text);
+      status.appendChild(statusText);
+      row.appendChild(status);
+
+      const f3 = document.createElement('td');
+      f3.textContent = record.f3;
+      row.appendChild(f3);
+
+      const date = document.createElement('td');
+      const dateValue = record.date;
+      const datemm = convertDateFormat(dateValue)
+      const dateNumericValue = Date.parse(datemm);
+      const days = Math.floor((Date.now() - dateNumericValue) / (1000 * 60 * 60 * 24)) + " days";
+      date.textContent = days
+      date.setAttribute('data', dateNumericValue);
+      row.appendChild(date);
+
+      // Add event listener to show full date on hover
+      date.addEventListener('mouseover', () => {
+        date.textContent = dateValue;
+      });
+
+      // Add event listener to show days count on mouseout
+      date.addEventListener('mouseout', () => {
+        date.textContent = days;
+      });
+
+      // Add event listener to row to go to the link on click
+      row.addEventListener('click', () => {
+        window.open(record.link, '_blank');
+      });
+
+      // Add event listener to status to go to the status link on click
+      status.addEventListener('click', (event) => {
+        event.stopPropagation();
+        window.open(record.status.link, '_blank');
+      });
+
+      // Add row to tbody
+      tbody.appendChild(row);
+    });
+  });
+
+// Format time in mm:ss.xxx format
+function formatTime(milliseconds) {
+  const minutes = Math.floor(milliseconds / 60000);
+  const seconds = Math.floor((milliseconds - minutes * 60000) / 1000);
+  const millisecondsRemainder = milliseconds - minutes * 60000 - seconds * 1000;
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${millisecondsRemainder.toString().padStart(3, '0')}`;
+}
+
+function convertDateFormat(dateString) {
+  // Split the date string into day, month, and year parts
+  var parts = dateString.split('/');
+  var day = parts[0];
+  var month = parts[1];
+  var year = parts[2];
+
+  // Create a new date string in mm/dd/yyyy format
+  var newDateString = month + '/' + day + '/' + year;
+
+  return newDateString;
+}

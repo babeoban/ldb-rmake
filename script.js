245f3124fde274f845b5f8b29f59b174ac9b3add
BuildTools <unconfigured@null.spigotmc.org>
2023-04-22 17:53:37 +0700

    too much features

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Load JSON data
-fetch('data.json')
+fetch('rsg.json')
   .then(response => response.json())
   .then(data => {
     // Sort data by time
@@ -117,3 +117,131 @@ function convertDateFormat(dateString) {
 
   return newDateString;
 }
+
+function loadData(file) {
+  fetch(file)
+    .then(response => response.json())
+    .then(data => {
+      // Process the data, e.g. display it in a table or list
+      const tbody = document.querySelector('#my-table tbody');
+      tbody.innerHTML = '';
+
+      data.sort((a, b) => a.time - b.time);
+
+      // Add rows to table
+      data.forEach((record, index) => {
+        // Create new row
+        const row = document.createElement('tr');
+  
+        // Add columns to row
+        const rank = document.createElement('td');
+        rank.textContent = index + 1;
+        row.appendChild(rank);
+  
+        const name = document.createElement('td');
+        name.textContent = record.name;
+        row.appendChild(name);
+  
+        const time = document.createElement('td');
+        time.textContent = formatTime(record.time);
+        row.appendChild(time);
+  
+        const status = document.createElement('td');
+        const statusText = document.createTextNode(record.status.text);
+        status.appendChild(statusText);
+        row.appendChild(status);
+  
+        const f3 = document.createElement('td');
+        f3.textContent = record.f3;
+        row.appendChild(f3);
+  
+        const date = document.createElement('td');
+        const dateValue = record.date;
+        const datemm = convertDateFormat(dateValue)
+        const dateNumericValue = Date.parse(datemm);
+        const elapsedTime = Date.now() - dateNumericValue;
+        
+        if (elapsedTime >= (1000 * 60 * 60 * 24 * 365)) {
+          const years = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 365)) + " years";
+          date.textContent = years;
+        } else if (elapsedTime >= (1000 * 60 * 60 * 24 * 30)) {
+          const months = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 30)) + " months";
+          date.textContent = months;
+        } else if (elapsedTime >= (1000 * 60 * 60 * 24 * 7)) {
+          const weeks = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 7)) + " weeks";
+          date.textContent = weeks;
+        } else {
+          const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24)) + " days";
+          date.textContent = days;
+        }
+        
+        date.setAttribute('data', dateNumericValue);
+        row.appendChild(date);
+        
+        // Add event listener to show full date on hover
+        row.addEventListener('mouseover', () => {
+          date.textContent = dateValue;
+        });
+        
+        // Add event listener to show elapsed time on mouseout
+        row.addEventListener('mouseout', () => {
+          if (elapsedTime >= (1000 * 60 * 60 * 24 * 365)) {
+            const years = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 365)) + " years";
+            date.textContent = years;
+          } else if (elapsedTime >= (1000 * 60 * 60 * 24 * 30)) {
+            const months = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 30)) + " months";
+            date.textContent = months;
+          } else if (elapsedTime >= (1000 * 60 * 60 * 24 * 7)) {
+            const weeks = Math.floor(elapsedTime / (1000 * 60 * 60 * 24 * 7)) + " weeks";
+            date.textContent = weeks;
+          } else {
+            const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24)) + " days";
+            date.textContent = days;
+          }
+        });
+        
+        
+        
+  
+        // Add event listener to row to go to the link on click
+        row.addEventListener('click', () => {
+          window.open(record.link, '_blank');
+        });
+  
+        // Add event listener to status to go to the status link on click
+        status.addEventListener('click', (event) => {
+          event.stopPropagation();
+          window.open(record.status.link, '_blank');
+        });
+  
+        // Add row to tbody
+        tbody.appendChild(row);
+      });
+
+    })
+    .catch(error => console.error(error));
+}
+
+const rsgButton = document.getElementById('rsg-button');
+rsgButton.addEventListener('click', () => {
+  loadData('rsg.json');
+});
+
+const ssgButton = document.getElementById('ssg-button');
+ssgButton.addEventListener('click', () => {
+  loadData('ssg.json');
+});
+
+rsgButton.addEventListener("click", () => {
+  rsgButton.classList.add("green-button");
+  rsgButton.classList.remove("black-button");
+  ssgButton.classList.add("black-button");
+  ssgButton.classList.remove("green-button");
+});
+
+ssgButton.addEventListener("click", () => {
+  ssgButton.classList.add("green-button");
+  ssgButton.classList.remove("black-button");
+  rsgButton.classList.add("black-button");
+  rsgButton.classList.remove("green-button");
+});
